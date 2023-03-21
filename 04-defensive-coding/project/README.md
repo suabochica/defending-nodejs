b878153d1bf298ea1041a49864cff989823801ff
Sergio Benítez <sergio.kun21@gmail.com>
2023-03-21 17:28:12 -0500

    defending-js: add project skeleton

---
+++ 04-defensive-coding/project/README.md
@@ -0,0 +1,6 @@
+# Trey Coding: Defensive Coding in JavaScript
+
+Congrats! You were just hired as a junior web developer at Trey Corp! Trey Corp has been around for several decades. Over the years, developers rarely had the opportunity to maintain their code. That’s where you come in - your first assignment at Trey Corp is to update some of the legacy code.
+
+The code you’ve been tasked to update is a Node.js express server that hosts web pages consisting of various utilities. Senior devs want you to find and fix any vulnerabilities you come across to improve overall security. Good luck!
+
---
+++ 04-defensive-coding/project/src/calculator.js
@@ -0,0 +1,29 @@
+import fs from "fs";
+// Import safe-eval
+
+
+export default function calculator_callback(request, response, response) {
+    const expression = request.query?.expressionChooser;
+    let content = "";
+
+    if (expression) {
+        try {
+            content = fs.readFileSync("public/calculator.html", "utf8");
+            // Replace eval with safe-eval;
+            const answer = eval(expression);
+            content = content.replace("&gt;", "&gt; " + answer)
+            response.send(content);
+        } catch (e) {
+            content = content.replace("&gt;", "&gt; " + e)
+            response.send(content);
+        }
+    } else {
+        try {
+            content = fs.readFileSync("public/calculator.html", "utf8");
+        } catch (e) {
+            console.log("Error:", e.stack);
+        }
+        response.send(content);
+    }
+}
+
