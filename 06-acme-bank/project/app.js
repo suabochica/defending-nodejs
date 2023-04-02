d2e04beeb724ba61d2ae4298adae293270554176
Sergio Benítez <sergio.kun21@gmail.com>
2023-04-02 12:49:26 -0500

    acme-bank: sanitize and validate inputs on the public form.

--- 06-acme-bank/project/app.js
+++ 06-acme-bank/project/app.js
@@ -4,6 +4,7 @@ const session = require("express-session");
 const path = require("path");
 const fs = require("fs");
 const helmet = require("helmet")
+const validator = require("express-validator")
 
 const db = new sqlite3.Database("./bank_sample.db");
 
@@ -166,7 +167,7 @@ app.get("/public_forum", function (request, response) {
 
 app.post("/public_forum", function (request, response) {
   if (request.session.loggedin) {
-    var comment = request.body.comment;
+    var comment = validator.escape(request.body.comment);
     var username = request.session.username;
     if (comment) {
       db.all(
